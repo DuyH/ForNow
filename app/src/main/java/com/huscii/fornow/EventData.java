4bee8a7b14ef5815ffbbbc7d19303698990a5bbc
Ian McPeek <iansmcpeek@gmail.com>
2015-06-28 05:19:06 -0700

    -added support for event data

--- app/src/main/java/com/huscii/fornow/EventData.java
+++ app/src/main/java/com/huscii/fornow/EventData.java
@@ -24,7 +24,41 @@ public class EventData {
         this.lat = lat;
         this.lon = lon;
     }
+    public String getTitle() {
+        return title;
+    }
 
+    public String getTag() {
+        return tag;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public boolean isLate() {
+        return late;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
     public void setLate() {
         if ((new Date()).getTime() > (created.getTime() + (time * 60 * 1000))) {
             this.late = true;
